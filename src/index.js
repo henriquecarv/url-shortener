73d6ff827e2d274081736816c9517a847f3f803a
Henrique Carvalho da Cruz <henriquecarv.cruz@gmail.com>
2018-12-09 20:17:56 -0200

    Move dotenv into the code

--- src/index.js
+++ src/index.js
@@ -3,15 +3,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const router = require('./routes/UrlShortRoute');
+require('dotenv').config();
 
 const app = express();
-const port = 3000;
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(router);
 
+const port = process.env.PORT;
+const address = process.env.ADDRESS;
+
 app.listen(port, () =>
   // eslint-disable-next-line no-console
-  console.info(`url-shortener listening on http://127.0.0.1:${port}/`)
+  console.info(`url-shortener listening on ${address}:${port}/`)
 );
